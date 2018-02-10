91a7710ce91669564417c1d297e6108d56e3d355
Paramveer Soundane <paramveersoundane@gmail.com>
2018-02-10 10:23:42 +0530

    Update chess.c

--- chess.c
+++ chess.c
@@ -5,7 +5,32 @@
 #include <assert.h>
 #include <limits.h>
 #include <stdbool.h>
+/* *Ways to give a check (chess.c)
+Sample Input.
 
+2
+########
+#k#P####
+########
+########
+########
+########
+#K######
+########
+########
+######P#
+####k###
+########
+########
+########
+####K###
+########
+Constraints:
+->There are at most 4 pieces of each color on the board
+->Each player has exactly one King and the given position on the board is a valid chess position assuming that it's White's move
+->It is guaranteed that White has exactly one Pawn in the 7th rank that can be promoted in a single move
+
+*/
 int waysToGiveACheck(char board[8][9]) {
     int check=0,l,i,j,c=1,m=0,n=0;
     int N[][2]={{2,1},{1,2},{2,-1},{1,-2}};
